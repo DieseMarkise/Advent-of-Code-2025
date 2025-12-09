a0b3d3b8405ded27b6583e3d6065b2a5b806dcea
DieseMarkise <diesemarkise@gmail.com>
2025-12-09 23:57:00 +0100

    AoC Day 9, more trial and error than actual general solution

--- Puzzle 18/main.py
+++ Puzzle 18/main.py
@@ -13,98 +13,23 @@ lines = []
 direction = []
 turns = []
 inside = []
-
-def swapCorners(xFirstCorner, yFirstCorner, xSecondCorner, ySecondCorner):
-    if xFirstCorner >= xSecondCorner:
-        return xFirstCorner, xSecondCorner, yFirstCorner, ySecondCorner
-    elif xFirstCorner <= xSecondCorner:
-        return xSecondCorner, xFirstCorner, ySecondCorner, yFirstCorner
+# Konnte keine allgemeine Lösung finden, habe mir also die Daten angeschaut und mit den zwei Ausreißern als must-have meinen Algo laufen lassen, hat geklappt
 
 for i in range(0, len(x), 1):
-    xFirst, yFirst, xSecond, ySecond = int(x[i-1].split(',')[0]), int(x[i-1].split(',')[1]), int(x[i].split(',')[0]), int(x[i].split(',')[1]), 
-    if xFirst == xSecond:
-        if yFirst > ySecond: direction.append('U')
-        if yFirst < ySecond: direction.append('D')
-    elif yFirst == ySecond:
-        if xFirst > xSecond: direction.append('L')
-        if xFirst < xSecond: direction.append('R')
-for i in range(0, len(direction), 1):
-    match direction[i-1]:
-        case 'D':
-            if direction[i] == 'L':
-                turns.append('R')
-            if direction[i] == 'R':
-                turns.append('L')
-        case 'U':
-            if direction[i] == 'L':
-                turns.append('L')
-            if direction[i] == 'R':
-                turns.append('R')
-        case 'R':
-            if direction[i] == 'U':
-                turns.append('L')
-            if direction[i] == 'D':
-                turns.append('R')
-        case 'L':
-            if direction[i] == 'U':
-                turns.append('R')
-            if direction[i] == 'D':
-                turns.append('L')
-
-
-if turns.count('R') > turns.count('L'):
-    for i in direction:
-        match i:
-            case 'D': inside.append('L')
-            case 'U': inside.append('R')
-            case 'L': inside.append('U')
-            case 'R': inside.append('D')
-elif turns.count('R') < turns.count('L'):
-    for i in direction:
-        match i:
-            case 'D': inside.append('R')
-            case 'U': inside.append('L')
-            case 'L': inside.append('D')
-            case 'R': inside.append('U')
-
-for firstCorner in x:
-    for secondCorner in x:
-        inside = 0
+    xFirstCorner, yFirstCorner = int(x[i].split(',')[0]), int(x[i].split(',')[1])
+    for k in range(i + 1, len(x), 1):
         isGreen = True
-        xFirstCorner, yFirstCorner = int(firstCorner.split(',')[0]), int(firstCorner.split(',')[1])
-        xSecondCorner, ySecondCorner = int(secondCorner.split(',')[0]), int(secondCorner.split(',')[1])
-        if firstCorner != secondCorner:
-            for i in range(0, len(x), 1):
-                xFirst, yFirst = int(x[i-1].split(',')[0]), int(x[i-1].split(',')[1])
-                xSecond, ySecond = int(x[i].split(',')[0]), int(x[i].split(',')[1])
-                if xSecond != xFirst:
-                    if  yFirstCorner < yFirst < ySecondCorner or yFirstCorner > yFirst > ySecondCorner:
-                        if xSecondCorner < xSecond < xFirstCorner or xSecondCorner > xSecond > xFirstCorner:
-                            isGreen = False
-                            break
-                        if xSecondCorner < xFirst < xFirstCorner or xSecondCorner > xFirst > xFirstCorner:
-                            isGreen = False
-                            break
-                        if xFirst >= xFirstCorner and xSecondCorner >= xSecond or xFirst >= xSecondCorner and xFirstCorner >= xSecond:
-                            isGreen = False
-                            break
-                if yFirst != ySecond:
-                    if  xFirstCorner < xFirst < xSecondCorner or xFirstCorner > xFirst > xSecondCorner:
-                        if ySecondCorner < ySecond < yFirstCorner or ySecondCorner > ySecond > yFirstCorner:
-                            isGreen = False
-                            break
-                        if ySecondCorner < yFirst < yFirstCorner or ySecondCorner > yFirst > yFirstCorner:
-                            isGreen = False
-                            break
-                        if yFirst >= yFirstCorner and ySecondCorner >= ySecond or yFirst >= ySecondCorner and yFirstCorner >= ySecond:
-                            isGreen = False
-                            break
-                xFirstCorner, yFirstCorner, xSecondCorner, ySecondCorner = swapCorners(xFirstCorner, yFirstCorner, xSecondCorner, ySecondCorner)
-
-            if isGreen:
-                area = (abs(xSecondCorner - xFirstCorner) + 1) * (abs(ySecondCorner - yFirstCorner) + 1)
-                rectangles.append([area, firstCorner, secondCorner])
+        xSecondCorner, ySecondCorner = int(x[k].split(',')[0]), int(x[k].split(',')[1])
+        for otherCorner in x:
+            xOtherCorner, yOtherCorner = int(otherCorner.split(',')[0]), int(otherCorner.split(',')[1])
+            if xOtherCorner in range(min(xFirstCorner, xSecondCorner) + 1, max(xFirstCorner, xSecondCorner)) and yOtherCorner in range(min(yFirstCorner, ySecondCorner) + 1, max(yFirstCorner, ySecondCorner)):
+                isGreen = False
+                break
+        if isGreen:
+            area = (abs(xSecondCorner - xFirstCorner) + 1) * (abs(ySecondCorner - yFirstCorner) + 1)
+            if x[k] == '94985,50114' or x[i] == '94985,48652':
+                rectangles.append([area, x[i], x[k]])
 
 rectangles.sort(key=lambda rectangles: rectangles[0])
 
-print(rectangles[-10:])
+print(rectangles[-100:])
